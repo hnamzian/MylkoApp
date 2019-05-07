0e2273ded01046693c92f689b95f6fd6601cdd72
hnamzian <namazian87@gmail.com>
2019-05-07 19:19:42 +0430

    bug-fix change base url for sms routes

--- src/providers/sms.ts
+++ src/providers/sms.ts
@@ -3,10 +3,11 @@ import { Injectable, OnInit } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { map, catchError } from "rxjs/operators";
 import { environment as env } from "../config/environment.dev";
+import { SMSRQ, SMSRSP } from "../models/sms";
 
 @Injectable()
-export class ServicesProvider {
-  baseUrl = `${env.BASE_URL}/periodic-services`;
+export class SMSProvider {
+  baseUrl = `${env.BASE_URL}/sms`;
 
   constructor(public http: HttpClient) {}
 }
