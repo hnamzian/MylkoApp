3a76664bbecab8d2d26948a01bf4063c492feaa1
hnamzian <namazian87@gmail.com>
2019-05-07 19:20:24 +0430

    add getSMSToken to request for sms token by mobile number

--- src/providers/sms.ts
+++ src/providers/sms.ts
@@ -10,4 +10,15 @@ export class SMSProvider {
   baseUrl = `${env.BASE_URL}/sms`;
 
   constructor(public http: HttpClient) {}
+
+  getSMSToken(mobile): Observable<SMSRSP> {
+    let url = `${this.baseUrl}/`;
+
+    const httpOptions = {
+      "Access-Control-Allow-Origin": "*",
+      params: new HttpParams().set("mobile", mobile)
+    };
+
+    return this.http.get(url, httpOptions).pipe(map((result: SMSRSP) => result));
+  }
 }
