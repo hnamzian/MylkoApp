81c834e0b3a2383ddafe0d4af579009b097926bb
hnamzian <namazian87@gmail.com>
2019-05-07 19:33:40 +0430

    add IonicStorageModule and TokenStorage provider to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
 import { HttpClientModule } from "@angular/common/http";
+import { IonicStorageModule } from "@ionic/storage";
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
 import { AuthModule } from "../pages/auth/auth.module";
@@ -14,12 +15,13 @@ import { GoatsModule } from "../pages/goats/goats.module";
 import { UserModule } from "../pages/user/user.module";
 import { CoreModule } from "../pages/core/core.module";
 import { SMSProvider } from "../providers/sms";
+import { TokenStorage } from "../storage/token/token";
 
 @NgModule({
   declarations: [MyApp, HomePage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp), HttpClientModule, AuthModule, DairyModule, DevicesModule, EmployeesModule, GoatsModule, UserModule, CoreModule],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), HttpClientModule, IonicStorageModule, AuthModule, DairyModule, DevicesModule, EmployeesModule, GoatsModule, UserModule, CoreModule],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage],
-  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }, SMSProvider]
+  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }, SMSProvider, TokenStorage]
 })
 export class AppModule {}
