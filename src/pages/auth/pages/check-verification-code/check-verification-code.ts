6f295db542db3a73e5b0590f3df8566e7227ec4e
hnamzian <namazian87@gmail.com>
2019-05-07 23:30:36 +0430

    get params mobile and smsCode from register page

--- src/pages/auth/pages/check-verification-code/check-verification-code.ts
+++ src/pages/auth/pages/check-verification-code/check-verification-code.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { NavParams } from "ionic-angular";
 
 @Component({
   selector: "check-verification-code",
   templateUrl: "check-verification-code.html"
 })
 export class CheckVerificationCodePage {
-  constructor() {}
+  mobile;
+
+  constructor(public navParams: NavParams) {
+    // just for dev mode
+    const code = this.navParams.get("code");
+
+    this.mobile = this.navParams.get("mobile");
+
+    console.log(this.mobile, code);
+  }
 }
