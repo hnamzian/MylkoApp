12d3ccaa155839979bb8f409f87f99cefac0adf8
hnamzian <namazian87@gmail.com>
2019-05-08 17:45:41 +0430

    remove forget password page from auth.module

--- src/pages/auth/auth.module.ts
+++ src/pages/auth/auth.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from "@angular/common";
 import { IonicModule } from "ionic-angular";
 import { RegisterPage } from "./pages/register/register";
 import { LoginPage } from "./pages/login/login";
-import { ForgetPasswordPage } from "./pages/forget-password/forget-password";
 import { CheckVerificationCodePage } from "./pages/check-verification-code/check-verification-code";
 import { CoreModule } from "../core/core.module";
 
 @NgModule({
   imports: [CommonModule, IonicModule, CoreModule],
-  declarations: [RegisterPage, LoginPage, ForgetPasswordPage, CheckVerificationCodePage],
-  entryComponents: [RegisterPage, LoginPage, ForgetPasswordPage, CheckVerificationCodePage]
+  declarations: [RegisterPage, LoginPage, CheckVerificationCodePage],
+  entryComponents: [RegisterPage, LoginPage, CheckVerificationCodePage]
 })
 export class AuthModule {}
