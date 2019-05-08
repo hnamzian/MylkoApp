31db2a4c31bfa3aca234c3eb4459c0e089e00e2a
hnamzian <namazian87@gmail.com>
2019-05-08 17:56:37 +0430

    remove register page from imports list

--- src/pages/auth/auth.module.ts
+++ src/pages/auth/auth.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "ionic-angular";
-import { RegisterPage } from "./pages/register/register";
 import { LoginPage } from "./pages/login/login";
 import { CheckVerificationCodePage } from "./pages/check-verification-code/check-verification-code";
 import { CoreModule } from "../core/core.module";
 
 @NgModule({
   imports: [CommonModule, IonicModule, CoreModule],
-  declarations: [RegisterPage, LoginPage, CheckVerificationCodePage],
-  entryComponents: [RegisterPage, LoginPage, CheckVerificationCodePage]
+  declarations: [LoginPage, CheckVerificationCodePage],
+  entryComponents: [LoginPage, CheckVerificationCodePage]
 })
 export class AuthModule {}
