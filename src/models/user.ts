df896c51c91f256a7f8d6f1735797d32130022d6
hnamzian <namazian87@gmail.com>
2019-05-08 22:45:50 +0430

    add USER model

---
+++ src/models/user.ts
@@ -0,0 +1,10 @@
+export interface USER {
+  id: string;
+  firstName: string;
+  lastName: string;
+  mobile: string;
+  email: string;
+  address: string;
+  createdAt: string;
+  updatedAt: string;
+}
