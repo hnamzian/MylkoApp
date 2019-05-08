e6e27493328c738633db533a9913fa18f9e1c4b8
hnamzian <namazian87@gmail.com>
2019-05-08 23:19:47 +0430

    tyr login at app startup and nav to home page at success

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,24 @@ import { SplashScreen } from "@ionic-native/splash-screen";
 
 import { HomePage } from "../pages/home/home";
 import { LoginPage } from "../pages/auth/pages/login/login";
+import { AuthProvider } from "../providers/auth";
 
 @Component({
   templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage: any = LoginPage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public authProvider: AuthProvider) {
+    platform.ready().then(async () => {
+      let user$ = await authProvider.authByToken();
+      user$.subscribe(user => {
+        if (user.success) {
+          this.rootPage = HomePage;
+        } else {
+          this.rootPage = LoginPage;
+        }
+      });
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
