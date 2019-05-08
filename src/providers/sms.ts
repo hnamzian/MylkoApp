b411efb6437defe5211c9f9e51ea1533b4d61e05
hnamzian <namazian87@gmail.com>
2019-05-08 13:43:56 +0430

    add verifySMSToken to verify smsCode and smsToken

--- src/providers/sms.ts
+++ src/providers/sms.ts
@@ -4,16 +4,34 @@ import { Observable } from "rxjs/Rx";
 import { map, catchError } from "rxjs/operators";
 import { environment as env } from "../config/environment.dev";
 import { SMSRQ, SMSRSP } from "../models/sms";
+import { TokenStorage } from "../storage/token/token";
+import { API } from "../models/api";
 
 @Injectable()
 export class SMSProvider {
   baseUrl = `${env.BASE_URL}/sms`;
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient, public tokenStorage: TokenStorage) {}
 
   getSMSToken(mobile): Observable<SMSRSP> {
     let url = `${this.baseUrl}/`;
 
-    return this.http.post(url, {mobile}).pipe(map((result: SMSRSP) => result));
+    return this.http.post(url, { mobile }).pipe(map((result: SMSRSP) => result));
+  }
+
+  async verifySMSToken(smsCode) {
+    let url = `${this.baseUrl}/verify`;
+
+    const token = (await this.tokenStorage.getSMSToken()) || false;
+
+    if (!token) return Observable.of({} as API);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        SMSToken: token
+      })
+    };
+
+    return this.http.post(url, { smsCode }, httpOptions).pipe(map((result: API) => result));
   }
 }
