49e96dc91ab336f8a0bd02a2ae1943a8dfd8a2a4
hnamzian <namazian87@gmail.com>
2019-05-27 23:06:41 +0430

    move dairy provider to providers/dairy folder

--- src/providers/dairy/dairy.ts
+++ src/providers/dairy/dairy.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 import { Injectable, OnInit } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { map, catchError } from "rxjs/operators";
-import { environment as env } from "../config/environment.dev";
+import { environment as env } from "../../config/environment.dev";
 
 @Injectable()
 export class AuthProvider {
