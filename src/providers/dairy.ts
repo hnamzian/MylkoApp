281a979e48d96842434e52a8f69ea5bd194fc5c3
hnamzian <namazian87@gmail.com>
2019-05-27 23:04:27 +0430

    add dairy provider to manage /dairy requests

---
+++ src/providers/dairy.ts
@@ -0,0 +1,10 @@
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { Injectable, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { map, catchError } from "rxjs/operators";
+import { environment as env } from "../config/environment.dev";
+
+@Injectable()
+export class AuthProvider {
+  baseUrl = `${env.BASE_URL}/dairy`;
+}
