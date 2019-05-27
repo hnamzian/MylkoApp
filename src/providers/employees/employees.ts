e39e9761d721d4b7d31dbadf778201594e93be44
hnamzian <namazian87@gmail.com>
2019-05-28 00:37:42 +0430

    add getEmployeeById to request employee profile by id

--- src/providers/employees/employees.ts
+++ src/providers/employees/employees.ts
@@ -4,9 +4,25 @@ import { Observable } from "rxjs/Rx";
 import { map, catchError } from "rxjs/operators";
 import { environment as env } from "../../config/environment.dev";
 import { TokenStorage } from "../../storage/token";
+import { EmployeeResponse } from "../../models/employees";
 
 @Injectable()
 export class EmployeesProvider {
   baseUrl = `${env.BASE_URL}/employees`;
 
+  constructor(public http: HttpClient, public tokenStorage: TokenStorage) {}
+
+  async getEmployeeById(employeeId, dairyId): Promise<Observable<EmployeeResponse>> {
+    let url = `${this.baseUrl}/`;
+
+    const token = (await this.tokenStorage.getAuthToken()) || false;
+    if (!token) return Observable.of({} as EmployeeResponse);
+
+    const httpOptions = {
+      headers: new HttpHeaders({ Authorization: token }),
+      params: new HttpParams().set("employeeId", employeeId).set("dairyId", dairyId)
+    };
+
+    return this.http.get(url, httpOptions).pipe(map((result: EmployeeResponse) => result));
+  }
 }
