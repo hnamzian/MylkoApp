f6970985275abfc8586e5447cc09758b81c58636
hnamzian <namazian87@gmail.com>
2019-05-28 00:17:47 +0430

    add removeDairy to request server removing a dairy

--- src/providers/dairy/dairy.ts
+++ src/providers/dairy/dairy.ts
@@ -5,6 +5,7 @@ import { map, catchError } from "rxjs/operators";
 import { environment as env } from "../../config/environment.dev";
 import { TokenStorage } from "../../storage/token";
 import { DairyResponse, DairiesResponse, Dairy } from "../../models/dairy";
+import { API } from "../../models/api";
 
 @Injectable()
 export class AuthProvider {
@@ -63,6 +64,20 @@ export class AuthProvider {
     };
 
     return this.http.post(url, dairy, httpOptions).pipe(map((result: DairiesResponse) => result));
+  }
+
+  async removeDairy(dairyId) {
+    let url = `${this.baseUrl}/remove`;
+
+    const token = (await this.tokenStorage.getAuthToken()) || false;
+    if (!token) return Observable.of({} as API);
+
+    const httpOptions = {
+      headers: new HttpHeaders({ Authorization: token }),
+      params: new HttpParams().set("dairyId", dairyId)
+    };
+
+    return this.http.get(url, httpOptions).pipe(map((result: API) => result));
   }
 
 }
