69bcbee7f7fe695c2ada940b665e629df9278108
hnamzian <namazian87@gmail.com>
2019-05-28 00:27:25 +0430

    add employee interfaces

---
+++ src/models/employees.ts
@@ -0,0 +1,21 @@
+import { API } from "./api";
+
+export interface EmployeeResponse extends API {
+  employee: Employee;
+}
+
+export interface EmployeesResponse extends API {
+  employees: Employee[];
+}
+
+export interface Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  mobile: string;
+  email: string;
+  address: string;
+  position: string;
+  DairyId: number;
+  AdminId: number;
+}
