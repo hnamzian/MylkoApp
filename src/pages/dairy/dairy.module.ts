7efb6a89f6573f0ca737900bdaa5f054635334d1
hnamzian <namazian87@gmail.com>
2019-05-30 14:22:43 +0430

    register DairyProfilePage to dairy.module

--- src/pages/dairy/dairy.module.ts
+++ src/pages/dairy/dairy.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "ionic-angular";
+import { DairyProfilePage } from "./pages/dairy-profile/dairy-profile";
 import { DairyDetailStatsPage } from "./pages/dairy-detail-stats/dairy-detail-stats";
 import { DairyStatsPage } from "./pages/dairy-stats/dairy-stats";
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [DairyStatsPage, DairyDetailStatsPage],
-  entryComponents: [DairyStatsPage, DairyDetailStatsPage]
+  declarations: [DairyProfilePage, DairyStatsPage, DairyDetailStatsPage],
+  entryComponents: [DairyProfilePage, DairyStatsPage, DairyDetailStatsPage]
 })
 export class DairyModule {}
