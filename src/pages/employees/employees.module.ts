cbf4bc955eeb627fdc6e7457497c84e5ad93b04d
hnamzian <namazian87@gmail.com>
2019-05-31 12:12:30 +0430

    import core.module at employees.module

--- src/pages/employees/employees.module.ts
+++ src/pages/employees/employees.module.ts
@@ -4,9 +4,10 @@ import { IonicModule } from "ionic-angular";
 import { EmployeeProfilePage } from "./pages/employee-profile/employee-profile";
 import { EmployeeListPage } from "./pages/employees-list/employee-list";
 import { NewEmployeePage } from "./pages/new-employee/new-employee";
+import { CoreModule } from "../core/core.module";
 
 @NgModule({
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, CoreModule],
   declarations: [EmployeeProfilePage, EmployeeListPage, NewEmployeePage],
   entryComponents: [EmployeeProfilePage, EmployeeListPage, NewEmployeePage]
 })
