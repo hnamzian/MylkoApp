142c1b79486ae1be131b765349b70fa006e8b527
hnamzian <namazian87@gmail.com>
2019-05-31 12:11:53 +0430

    register TrashBarComponent at core.module

--- src/pages/core/core.module.ts
+++ src/pages/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "ionic-angular";
 import { AuthHeaderComponent } from "./components/auth-header/auth-header";
+import { TrashBarCompponent } from "./components/trash-bar/trash-bar";
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [AuthHeaderComponent],
-  entryComponents: [],
+  declarations: [AuthHeaderComponent, TrashBarCompponent],
+  entryComponents: [TrashBarCompponent],
   exports: [AuthHeaderComponent]
 })
 export class CoreModule {}
