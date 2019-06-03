6af56e6092e412c6d4693df8f28b8449aaf3f18e
hnamzian <namazian87@gmail.com>
2019-06-03 13:13:23 +0430

    import core.module into user.module

--- src/pages/user/user.module.ts
+++ src/pages/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "ionic-angular";
 import { ProfilePage } from "./pages/profile/profile";
+import { CoreModule } from "../core/core.module";
 
 @NgModule({
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, CoreModule],
   declarations: [ProfilePage],
   entryComponents: [ProfilePage]
 })
