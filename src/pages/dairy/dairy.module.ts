f74d92883f7a05ce466e02759e313e8a2c2434a2
hnamzian <namazian87@gmail.com>
2019-06-03 16:59:48 +0430

    import core.module in dairy.module

--- src/pages/dairy/dairy.module.ts
+++ src/pages/dairy/dairy.module.ts
@@ -4,9 +4,10 @@ import { IonicModule } from "ionic-angular";
 import { DairyProfilePage } from "./pages/dairy-profile/dairy-profile";
 import { DairyDetailStatsPage } from "./pages/dairy-detail-stats/dairy-detail-stats";
 import { DairyStatsPage } from "./pages/dairy-stats/dairy-stats";
+import { CoreModule } from "../core/core.module";
 
 @NgModule({
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, CoreModule],
   declarations: [DairyProfilePage, DairyStatsPage, DairyDetailStatsPage],
   entryComponents: [DairyProfilePage, DairyStatsPage, DairyDetailStatsPage]
 })
